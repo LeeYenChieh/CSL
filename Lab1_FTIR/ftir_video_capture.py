5c2038d0d061a002741323ca49689763449cd301
LeeYenCHieh <B10902113@ntu.edu.tw>
2023-10-06 17:13:35 +0800

    adjust threshld and add blur to avoid noise

--- Lab1_FTIR/ftir_video_capture.py
+++ Lab1_FTIR/ftir_video_capture.py
@@ -4,10 +4,10 @@ import time
 import math
 
 # Choose your webcam: 0, 1, ...
-cap = cv2.VideoCapture(0)
+cap = cv2.VideoCapture(2)
 
 fps = 60
-dist_thres = 50   # maximum distance to be identified as the same finger
+dist_thres = 100   # maximum distance to be identified as the same finger
 line_thickness = 50
 finger_hp = 5
 
@@ -15,9 +15,9 @@ def nothing(x):
 	pass
 
 cv2.namedWindow('Threshold Sliders')
-cv2.createTrackbar('R','Threshold Sliders',173,255,nothing)
-cv2.createTrackbar('G','Threshold Sliders',0,255,nothing)
-cv2.createTrackbar('B','Threshold Sliders',150,255,nothing)
+cv2.createTrackbar('R','Threshold Sliders',59,255,nothing)
+cv2.createTrackbar('G','Threshold Sliders',71,255,nothing)
+cv2.createTrackbar('B','Threshold Sliders',74,255,nothing)
 
 # a list of finger: a finger is a dictionary with key "hp" record its hp, delete the finger when hp reach 0. 
 # and key "pos" which is a list recording its positions in all time frame
@@ -36,11 +36,14 @@ while(True):
 	g_thresh = cv2.getTrackbarPos('G','Threshold Sliders')
 	b_thresh = cv2.getTrackbarPos('B','Threshold Sliders')
 	_,r = cv2.threshold(r,r_thresh,255,cv2.THRESH_BINARY)
-	_,g = cv2.threshold(g,g_thresh,255,cv2.THRESH_BINARY)
+	_,g_inv = cv2.threshold(g,g_thresh,255,cv2.THRESH_BINARY_INV)
 	_,b_inv = cv2.threshold(b,b_thresh,255,cv2.THRESH_BINARY_INV)
 
 	# Get the final result using bitwise operation
 	result = cv2.bitwise_and(r,b_inv,mask=None)
+	result = cv2.bitwise_and(result,g_inv,mask=None)
+	result = cv2.blur(result,(5,5))
+	_,result = cv2.threshold(result,128,255,cv2.THRESH_BINARY)
 
 	# Find and draw contours
 	contours, hierarchy = cv2.findContours(result,cv2.RETR_TREE,cv2.CHAIN_APPROX_SIMPLE)
@@ -49,7 +52,7 @@ while(True):
 	# Iterate through each contour, check the area and find the center
 	for contour in contours:
 		area = cv2.contourArea(contour)
-		if area > 2000:  # Adjust the minimum area threshold as needed
+		if area > 3000:  # Adjust the minimum area threshold as needed
 			(x, y), radius = cv2.minEnclosingCircle(contour)
 			center = (int(x), int(y))
 			cv2.circle(display, center, int(radius), (0, 255, 0), 2)  # Draw a green hollow circle
@@ -82,11 +85,11 @@ while(True):
 		finger["hp"] -= 1
 
 	# Show the frame
-	# zeros = np.zeros(frame.shape[:2],dtype="uint8")
-	# cv2.imshow('frame', frame)
-	# cv2.imshow("Red",cv2.merge([zeros,zeros,r]))
-	# cv2.imshow("Green",cv2.merge([zeros,g,zeros]))
-	# cv2.imshow("Blue",cv2.merge([b_inv,zeros,zeros]))
+	zeros = np.zeros(frame.shape[:2],dtype="uint8")
+	cv2.imshow('frame', frame)
+	cv2.imshow("Red",cv2.merge([zeros,zeros,r]))
+	cv2.imshow("Green",cv2.merge([zeros,g_inv,zeros]))
+	cv2.imshow("Blue",cv2.merge([b_inv,zeros,zeros]))
 	cv2.imshow("result",display)
 
 	# Press q to quit
